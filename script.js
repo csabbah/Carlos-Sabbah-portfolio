74960bac6c9445ec288d67b305f63e868d88764f
Csabbah <csabbah@ryerson.ca>
2022-02-28 07:20:26 -0500

    Added smooth scroll functions for nav link items

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const root = document.querySelector('body');
 
 const mainLanding = document.getElementById('landing');
 const landingSecondary = document.getElementById('landing-secondary');
-
 const personalCreatives = document.getElementById('personal-creatives');
 const webDevProjects = document.getElementById('webdev-projects');
+const contactForm = document.getElementById('contact-form');
 
 const brandTitle = document.getElementById('brand-title');
 const header = document.querySelector('header');
-const viewall = document.querySelector('.view-all-btn');
+
 // On page load (or when you hit refresh), scroll to the very top
 $(document).ready(function () {
   $(this).scrollTop(0);
@@ -21,15 +21,17 @@ $(document).ready(function () {
 });
 
 // ---- Scroll conditional
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', (e) => {
   const scroll = Math.ceil(this.scrollY);
-
   if (scroll > 150) {
     // Once you exceed 150, add the class of active to the landing page
     landingSecondary.classList.add('active'); // Active = Opacity 100%
   } else {
     // If you go under the condition, remove the class (aka, disappear)
     landingSecondary.classList.remove('active');
+  }
+
+  if (scroll < 1420) {
     root.style.backgroundColor = `rgb(0, 0, 0`;
   }
 
@@ -57,4 +59,41 @@ window.addEventListener('scroll', () => {
     root.style.backgroundColor = `rgb(${y}, ${y}, ${y})`;
     brandTitle.style.color = `rgb(${x}, ${x}, ${x})`;
   }
+});
+
+/* Scroll To */
+var element = document.getElementById('nav-what-i-do');
+element.addEventListener('click', () => {
+  landingSecondary.scrollIntoView({
+    behavior: 'smooth',
+    block: 'center',
+    inline: 'nearest',
+  });
+});
+
+var element2 = document.getElementById('nav-personal');
+element2.addEventListener('click', () => {
+  personalCreatives.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+    inline: 'nearest',
+  });
+});
+
+var element3 = document.getElementById('nav-webdev');
+element3.addEventListener('click', () => {
+  webDevProjects.scrollIntoView({
+    behavior: 'smooth',
+    block: 'center',
+    inline: 'nearest',
+  });
+});
+
+var element4 = document.getElementById('nav-form');
+element4.addEventListener('click', () => {
+  contactForm.scrollIntoView({
+    behavior: 'smooth',
+    block: 'center',
+    inline: 'nearest',
+  });
 });
